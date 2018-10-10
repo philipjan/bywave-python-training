9fcecd4244c53519f2f26d0960a696cb7d1929a7
xXLXx <leo.lavarias@gmail.com>
2018-10-10 13:18:02 +0800

    Add first assignment

---
+++ exer_001.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Write a program that accepts an input value (range: 60-100)
+and prints a message based on the following range of values:
+
+RANGE                   MESSAGE
+60 to 74                Derp!
+75 to 84                Good
+85 to 94                Very Good
+95 to 100               Level Asian!
+any other value         Invalid Value
+"""
+
+# your name and email address here
+__author__ = 'xXLXx <leo@bywave.com.au>'
+
+
+if __name__ == '__main__':
+    # your code here
